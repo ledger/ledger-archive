ccb132aabc8727a37d746280487f7db5fbf73f39
John Wiegley <johnw@newartisans.com>
2008-07-27 20:45:54 -0400

    Added script I used to perform Git branch surgery

---
+++ rewrite.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git filter-branch \
+    --env-filter 'test "$GIT_AUTHOR_NAME" = johnw -o "$GIT_AUTHOR_NAME" = johnw@newartisans.com -o "$GIT_AUTHOR_NAME" = jwiegley && export GIT_AUTHOR_NAME="John Wiegley" GIT_AUTHOR_EMAIL=johnw@newartisans.com GIT_COMMITTER_NAME="John Wiegley" GIT_COMMITTER_EMAIL=johnw@newartisans.com || cat' \
+    --index-filter 'git update-index --remove .cvsignore .huquq ChangeLog make.deps INSTALL TODO' \
+    --msg-filter "egrep -v '(darcs-hash|\<M\> |r[0-9]*@Hermes:)' | sed 's/\\*\\*\\*DARCS\\*\\*\\*//' | sed 's/empty log message/no comment/'" \
+    -d /var/log/johnw/work $1
