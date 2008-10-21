5b562f726c9e7ed3ff1ad3e2f38e9f6ef7ececca
John Wiegley <johnw@newartisans.com>
2008-10-21 14:07:34 -0600

    Added a few more new archive files.

--- rewrite.sh
+++ rewrite.sh
@@ -2,6 +2,6 @@
 
 git filter-branch \
     --env-filter 'test "$GIT_AUTHOR_NAME" = johnw -o "$GIT_AUTHOR_NAME" = johnw@newartisans.com -o "$GIT_AUTHOR_NAME" = jwiegley && export GIT_AUTHOR_NAME="John Wiegley" GIT_AUTHOR_EMAIL=johnw@newartisans.com GIT_COMMITTER_NAME="John Wiegley" GIT_COMMITTER_EMAIL=johnw@newartisans.com || cat' \
-    --index-filter 'git update-index --remove .cvsignore .huquq ChangeLog make.deps INSTALL TODO' \
+    --index-filter 'git update-index --remove *.pyc .cvsignore .arch-inventory' \
     --msg-filter "egrep -v '(darcs-hash|\<M\> |r[0-9]*@Hermes:)' | sed 's/\\*\\*\\*DARCS\\*\\*\\*//' | sed 's/empty log message/no comment/'" \
     $1
